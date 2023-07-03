06e421ef751fb34a4c200a72f51b0aedd203c9e8
jegazx <xjegazx@gmail.com>
2023-07-03 10:09:15 +0300

    modeliai

--- sportoclub/core/models.py
+++ sportoclub/core/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
+from sportoclub.user_profile.models import User
 
 class Exercise(models.Model):
-
-    
+    name = models.CharField(max_length=200)
+    video_url = models.URLField()
 
     class Meta:
         verbose_name = _("exercise")
@@ -15,3 +18,50 @@ class Exercise(models.Model):
     def get_absolute_url(self):
         return reverse("exercise_detail", kwargs={"pk": self.pk})
 
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    exercises = models.ManyToManyField(Exercise, related_name="WorkoutExercises")
+
+    class Meta:
+        verbose_name = _("workout")
+        verbose_name_plural = _("workouts")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("workout_detail", kwargs={"pk": self.pk})
+    
+class WorkoutExercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.PositiveIntegerField()
+    reps_per_set = models.PositiveIntegerField()  
+
+    class Meta:
+        verbose_name = _("workoutexercise")
+        verbose_name_plural = _("workoutexercises")
+
+    def __str__(self):
+        return self.exercise.name
+
+    def get_absolute_url(self):
+        return reverse("workoutexercise_detail", kwargs={"pk": self.pk})
+
+class SetLog(models.Model):
+    workou_exercise = models.ForeignKey(WorkoutExercise, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    weight = models.PositiveIntegerField()
+    reps = models.PositiveIntegerField()
+    
+
+    class Meta:
+        verbose_name = _("setlog")
+        verbose_name_plural = _("setlogs")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("setlog_detail", kwargs={"pk": self.pk})
