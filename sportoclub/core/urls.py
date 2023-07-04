66981714a9f95f4467bb0aab0e28acd064b8bde1
jegazx <xjegazx@gmail.com>
2023-07-04 09:06:11 +0300

    exercise

--- sportoclub/core/urls.py
+++ sportoclub/core/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from .views import ExerciseListView, ExerciseDetailView, ExerciseCreateView, ExerciseUpdateView
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('exercises/', ExerciseListView.as_view(), name='exercise_list'),
+    path('exercises/<int:pk>/', ExerciseDetailView.as_view(), name='exercise_detail'),
+    path('exercises/new/', ExerciseCreateView.as_view(), name='exercise_create'),
+    path('exercises/<int:pk>/edit/', ExerciseUpdateView.as_view(), name='exercise_edit'),
+
 ]
--- sportoclub/core/views.py
+++ sportoclub/core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Exercise
 
 # Create your views here.
 def index(request):
@@ -8,3 +11,22 @@ def index(request):
     else:
         # User is not logged in
         return render(request, 'index.html')
+    
+
+class ExerciseListView(ListView):
+    model = Exercise
+    template_name = 'exercise_list.html'
+
+class ExerciseDetailView(DetailView):
+    model = Exercise
+    template_name = 'exercise_detail.html'
+
+class ExerciseCreateView(CreateView):
+    model = Exercise
+    fields = ('name', 'video_url',) 
+    template_name = 'exercise_form.html'
+
+class ExerciseUpdateView(UpdateView):
+    model = Exercise
+    fields = ('name', 'video_url',)
+    template_name = 'exercise_form.html'
